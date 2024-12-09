64e37e033d1765abe904f3648bbf4cdfedb87dbb
Letícia Zalasik <leticiazalasik@gmail.com>
2024-12-09 15:38:21 -0300

    :sparkles: Fix: CRUD categoria.

---
+++ src/app/admin/categorias/AddCategoryButton.tsx
@@ -0,0 +1,32 @@
+// app/admin/categorias/AddCategoryButton.tsx
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { CategoryForm } from "./CategoryForm";
+
+export function AddCategoryButton() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>Adicionar Categoria</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Nova Categoria</DialogTitle>
+        </DialogHeader>
+        <CategoryForm onSuccess={() => setOpen(false)} />
+      </DialogContent>
+    </Dialog>
+  );
+}
